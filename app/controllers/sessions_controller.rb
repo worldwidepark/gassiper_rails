b31b31c32dd3a7ea4f121c1864dd237eb1d3b096
worldwidepark <qkrguddn1066@naver.com>
2022-05-01 01:46:03 +0900

    add scss

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,26 +5,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
     user = User.find_by_email(params[:email]) 
     if user && user.valid_password?(params[:password])
       sign_in(:user, user)
       session[:user_id] = user.id
       redirect_to posts_path
     else
-      sign_out(user)
       render new_session_path
     end
   end
   
   def destroy
-    @user = User.find_by_email(session[:user_id])
+    @user = User.find_by_email(current_user.id)
     sign_out(@user)
     redirect_to posts_path 
   end
 
 
   def user_params
-    params.permit(:email)
+    params.permit(:email, :password)
   end
 
 end
