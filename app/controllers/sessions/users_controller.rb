fd53860425ca3ecc17a6a4ebb4e146052c01c6e9
worldwidepark <qkrguddn1066@naver.com>
2022-05-06 15:13:28 +0900

    test for create_user by using original devise controller

--- app/controllers/sessions/users_controller.rb
+++ app/controllers/sessions/users_controller.rb
@@ -5,12 +5,12 @@ class Sessions::UserController < Devise::RegistrationsController
   end
   
   def create
-    @user= User.new(user_params)
-    if @user.save
-      redirect_to posts_path  
-    else
-      render new_user_path
-    end
+    # @user= User.new(user_params)
+    # if @user.save
+    #   redirect_to posts_path  
+    # else
+    #   render new_user_path
+    # end
   end
 
   def logout
@@ -22,6 +22,7 @@ class Sessions::UserController < Devise::RegistrationsController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :name )
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
+
 end
