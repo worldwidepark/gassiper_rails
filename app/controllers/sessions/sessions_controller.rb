d9ffbbce4b5794e41c6866be84714ad60c4c86d3
worldwidepark <qkrguddn1066@naver.com>
2022-05-09 00:49:30 +0900

    edit scss

--- app/controllers/sessions/sessions_controller.rb
+++ app/controllers/sessions/sessions_controller.rb
@@ -1,34 +1,11 @@
 class Sessions::SessionsController < Devise::SessionsController
 
-  # def new
-  #   @user = User.new
-  # end
-
   def create
     self.resource = warden.authenticate!(auth_options)
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
     yield resource if block_given?
     redirect_to after_sign_in_path_for(resource)
-      #   # @user = User.new(user_params)
-  #   # user = User.find_by_email(params[:email]) 
-  #   # if user && user.valid_password?(params[:password])
-  #   #   sign_in(:user, user)
-  #   #   session[:user_id] = user.id
-  #   #   redirect_to posts_path
-  #   # else
-  #   #   render 'devise/sessions/new'
-  #   # end
-    end 
+  end
 
-  # def destroy
-  #   # @user = User.find_by_email(current_user.id)
-  #   # sign_out(@user)
-  #   # redirect_to posts_path 
-  # end
-
-
-  # def user_params
-  #   params.permit(:email, :password)
-  # end
 end
--- app/controllers/sessions/users_controller.rb
+++ app/controllers/sessions/users_controller.rb
@@ -1,28 +1,2 @@
 class Sessions::UsersController < Devise::RegistrationsController
-  # def new
-  #   # @user = User.new
-  #   # @user_params
-  # end
-  
-  # def create
-  #   # @user= User.new(user_params)
-  #   # if @user.save
-  #   #   redirect_to posts_path  
-  #   # else
-  #   #   render new_user_path
-  #   # end
-  # end
-
-  # def logout
-  # end
-
-  # def show
-  # end
-
-  # # private
-
-  # # def user_params
-  # #   params.require(:user).permit(:email, :password, :password_confirmation, :name)
-  # # end
-
 end
