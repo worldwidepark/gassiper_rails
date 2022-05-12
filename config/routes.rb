befe921d7f49f47a5b6da1753bf00fb4ea87985f
worldwidepark <qkrguddn1066@naver.com>
2022-05-12 22:12:05 +0900

    merge create session

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,6 @@ Rails.application.routes.draw do
     :passwords => 'users/passwords' ,
     :registrations => 'sessions/registrations'
   }
-<<<<<<< HEAD
-  devise_scope :user do
-    get "/registrations/show" => "sessions/registrations#show"
-    # patch "/registrations/patch" => "sessions/registrations#update"
-    end
-  
-  # get 'user/show', to: 'sessions/users#show' 
-  # resources :sessions , only: [:new, :create, :destroy]
-=======
->>>>>>> origin/create_session
   resources :posts , only: [:index, :create, :edit]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
