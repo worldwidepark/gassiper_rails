52290ac594754a4581134ebf7c7d0d540b8bbe08
worldwidepark <qkrguddn1066@naver.com>
2022-05-21 03:21:55 +0900

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,20 @@
 class CommentsController < ApplicationController
   def new
     @post= Post.find(params[:post_id])
+    @comment= @post.comments.new()
   end
 
   def create
     @comment = Comment.new(post_params)
     if @comment.save
-      redirect_to post_path(params[:post_id])
+      redirect_to post_path(@comment.post_id), notice: "コメントが作成されました。"
     else
-      redirect_to new_comment_path(post_id: params[:post_id]) , alert: "投稿失敗！140文字までです。"
+      render new_comment_path(post_id: @comment.post_id)
     end
   end
 
   def post_params    
-    params.permit(:text,:user_id,:post_id)
+    params.require(:comment).permit(:text,:user_id,:post_id)
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :text, length: { minimum: 1, maximum: 140 }
+  validates :text, presence: true, length: { minimum: 1, maximum: 140 }
   belongs_to :user
   belongs_to :post
 end
