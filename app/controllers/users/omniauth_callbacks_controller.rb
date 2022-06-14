ccd4d6c9a3b7ab350fb27fdac488c77190c45cf8
worldwidepark <qkrguddn1066@naver.com>
2022-06-14 21:34:43 +0900

    -

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,13 @@
-# Deviseのコントローラーを継承することでDeviseのコントローラーと同じメソッド（つまりアクション）が使える
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    # request.env["omniauth.auth"]にGitHubから送られてきたデータが入っている
-    # binding.pryで確認してみましょう
+
     @user = User.find_for_github_oauth(request.env["omniauth.auth"])
 
     unless @user.deleted_flag
-      if @user.persisted? # データベースに保存されていればログイン成功
+      if @user.persisted?
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
-      else # ログイン失敗
+      else
         session["devise.github_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
-
   validates :name, presence: true
+
   def self.find_for_github_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
-      # 名前を取得するときはこのように書く（今回はUserモデルにname属性がないのでエラーなる）
       user.name = auth.info.name
       user.email = auth.info.email
-      # 任意の20文字の文字列を作成する
       user.password = Devise.friendly_token[0, 20]
     end
   end
-  # 最後に使います。
-  def self.create_unique_string
-    SecureRandom.uuid
-  end
+
   has_many :posts
   has_many :comments
   has_one_attached :profile_picture
