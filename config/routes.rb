c76f500a0c7227bf9410e3087cc698b61413fd7a
worldwidepark <qkrguddn1066@naver.com>
2022-06-18 22:04:02 +0900

    route modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
   root to: 'posts#index'
-  resources :users , only: [:show, :update] , path: 'users/show'
+
   devise_for :users,
     :controllers => {
     :omniauth_callbacks => "users/omniauth_callbacks",
     :registrations => 'sessions/registrations',
     :sessions => 'sessions/sessions'
   }
-
+  resources :users , only: [:show, :update]
   resources :posts , only: [:index, :new, :create,:show, :destroy] do
     resources :comments , only: [:new, :create, :destroy]
   end
