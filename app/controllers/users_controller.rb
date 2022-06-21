10f205aa6a02d944ba66f186dbc8c3d2ee05d462
worldwidepark <qkrguddn1066@naver.com>
2022-06-22 00:31:11 +0900

    css - like for comment, user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
-    @comments = @user.comments
+    @posts = Post.all
+    @comments = Comment.all
   end
 
   def update
