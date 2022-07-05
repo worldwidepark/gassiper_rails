46374b9e1f6e87b67175627a9d743d742cded01a
worldwidepark <qkrguddn1066@naver.com>
2022-07-05 23:21:23 +0900

    just small modify

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,19 +1,21 @@
 class FollowsController < ApplicationController
   def create
-    user= User.find(current_user.id)
-    follow = user.follows.new(followed_id: params[:want_follow_user_id])
+    follow = current_user.follows.new(followed_id: params[:want_follow_user_id])
     if follow.save
       redirect_to user_path(params[:want_follow_user_id]), notice: "フォローしました。"
+    else
+      redirect_to user_path(params[:want_follow_user_id]), alert: "フォローが失敗しました。"
     end
   end
 
   def destroy
     user= User.find(current_user.id)
     follow_info = user.follows.find_by(followed_id: params[:want_unfollow_user_id])
-    follow_info.delete
-    redirect_to user_path(params[:want_unfollow_user_id]), alert: "フォローをやめました。"
+    if follow_info.delete
+      redirect_to user_path(params[:want_unfollow_user_id]), notice: "フォローを解除しました。"
+    else
+      redirect_to user_path(params[:want_follow_user_id]), alert: "フォローの解除が失敗しました。"
+    end
   end
-
-
 
 end
