dd2758de67f619c3a3a487403bc4cd8f04003ba6
worldwidepark <qkrguddn1066@naver.com>
2022-07-05 22:27:03 +0900

    add follow to post/ controller and index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if current_user
+      @posts = Post.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
+      @posts_unfollowed = Post.where.not(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
+    end
   end
 
   def new
